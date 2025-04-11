ea469ae5aa90a432de107d2089ddcfd6293264be
Sangeetha3112ks <kssangeetha3112@gmail.com>
2025-04-11 15:34:12 +0530

    content js and css changed

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import './Content.css'
+import React from 'react';
+import './Content.css';
+
 const Content = () => {
   return (
-    <div className='contents'> 
-    <h3>Insta Feed</h3>
-        <p>Follow us on instagram.
-            <a href="#daxone">@Daxone</a>
-        </p>
+    <div className="contents">
+      <h3>Insta Feed</h3>
+      <p>
+        Follow us on instagram.
+        <a href="#daxone">@Daxone</a>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
